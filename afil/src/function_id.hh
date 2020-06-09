8ca76f097ce95a609c128194817fa2e8e0bc7cdd
asielorz <asielorz@gmail.com>
2020-06-09 14:47:58 +0200

    Added deleted function id

--- afil/src/function_id.hh
+++ afil/src/function_id.hh
@@ -1,6 +1,5 @@
 #pragma once
 
-
 struct FunctionId
 {
 	enum struct Type : unsigned { program, imported, intrinsic };
@@ -10,9 +9,9 @@ struct FunctionId
 
 	Type type : 2;
 	unsigned index : 30;
-
 };
 constexpr FunctionId invalid_function_id = {FunctionId::Type::intrinsic, (1u << 30u) - 1u};
+constexpr FunctionId deleted_function_id = {FunctionId::Type::intrinsic, (1u << 30u) - 2u};
 constexpr FunctionId pointer_equal_intrinsic = {FunctionId::Type::intrinsic, 100}; // TODO: Depends on pointer size
 constexpr FunctionId pointer_three_way_compare_intrinsic = {FunctionId::Type::intrinsic, 101};
 
